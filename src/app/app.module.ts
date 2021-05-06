8b3463542a517ddf2a43683b92a6ee9e102dffd0
Nazım Marul <nazmmarul@hotmail.com>
2021-05-06 14:20:49 +0300

    Refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'; //backende bağlanmak için gerekli.
-import {FormsModule} from "@angular/forms"; //[(ngModel)] ı kullanabilmek için gerekli.
+import {FormsModule, ReactiveFormsModule} from "@angular/forms"; //[(ngModel)] ı kullanabilmek için gerekli. Ve add işlemi için reactiveFormsModuleyi ekledik
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations" // ngx-toastr ile animasyonu güzel kullanmak için eklendi.
 
 import { AppRoutingModule } from './app-routing.module';
@@ -15,6 +15,7 @@ import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
 import {ToastrModule} from "ngx-toastr"; // ngx-toastr ı kullanabilmek için eklendi.
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
 
 
 @NgModule({
@@ -26,6 +27,7 @@ import { CartSummaryComponent } from './components/cart-summary/cart-summary.com
     VatAddedPipe,
     FilterPipePipe,
     CartSummaryComponent,
+    ProductAddComponent,
 
   ],
   imports: [
@@ -36,7 +38,8 @@ import { CartSummaryComponent } from './components/cart-summary/cart-summary.com
     ToastrModule.forRoot({
       positionClass:"toast-bottom-right"
     }),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
